d9fdfabdb205d797b58dcf7c4e21cc14a9fe6073
uge02005 <uge02005@SBC-LP-0002.CENTRAL.HOTUSA.INT>
2019-08-20 17:16:25 +0200

    Empezando con la primera pantalla

--- munchkin(v1)/src/WelcomeWindow.java
+++ munchkin(v1)/src/WelcomeWindow.java
@@ -1,9 +1,13 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class WelcomeWindow extends JFrame {
 	// Atributos de la clase
 	private JPanel panel;
+	private JTextField tFNombreP;
 	
 	// Constructor vacío de la clase
 	public WelcomeWindow() {
@@ -15,5 +19,27 @@ public class WelcomeWindow extends JFrame {
 		panel = new JPanel();
 		panel.setLayout(null);
 		this.getContentPane().add(panel);
+		
+		JLabel lblLogo = new JLabel("LOGO");
+		lblLogo.setBounds(234, 68, 46, 14);
+		panel.add(lblLogo);
+		
+		JLabel lblEmpezarPartida = new JLabel("\u00BFQuieres empezar una nueva aventura?");
+		lblEmpezarPartida.setBounds(150, 131, 220, 14);
+		panel.add(lblEmpezarPartida);
+		
+		JLabel lblNombrePersonaje = new JLabel("\u00BFC\u00F3mo quieres que se llame el personaje?");
+		lblNombrePersonaje.setBounds(150, 156, 199, 14);
+		panel.add(lblNombrePersonaje);
+		
+		tFNombreP = new JTextField();
+		tFNombreP.setBounds(169, 193, 170, 20);
+		panel.add(tFNombreP);
+		tFNombreP.setColumns(10);
+		
+		JButton btnEmpezar = new JButton("\u00A1Empezar!");
+		btnEmpezar.setBounds(210, 241, 89, 23);
+		btnEmpezar.setEnabled(false);
+		panel.add(btnEmpezar);
 	}
 }
